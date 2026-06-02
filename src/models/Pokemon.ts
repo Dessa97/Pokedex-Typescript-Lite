bea9e15308262ace0f81236d6efee05cfbe7b9da
Dessa97 <andressaoliveira862@gmail.com>
2026-06-02 14:58:14 -0300

    feat: Criada interface PokemonResumo e interface PokemonApiResponse

--- src/models/Pokemon.ts
+++ src/models/Pokemon.ts
@@ -1,1 +1,19 @@
 //# Interfaces/Types e Classes de Entidade. Molde rigoroso dos atributos consumidos da API.
+
+//RF02 – Criar uma interface para o Pokémon resumido
+export interface PokemonResumo {
+  id: number;
+  nome: string;
+  tipos: string[];
+  altura: number;
+  peso: number;
+}
+
+//RF03 – Criar uma interface para o retorno da API
+interface PokemonApiResponse {
+  id: number;
+  name: string;
+  types: { type: { name: string } }[];
+  height: number;
+  weight: number;
+}
