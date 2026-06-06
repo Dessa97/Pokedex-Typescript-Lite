6c6948bb2851e4b75707d8a548e433352b0d879c
Dessa97 <andressaoliveira862@gmail.com>
2026-06-06 20:27:13 -0300

    feat: Adicionadas classes de erro personalizadas: APIError e LocalBoxError.

--- src/models/CustomErrors.ts
+++ src/models/CustomErrors.ts
@@ -1,1 +1,15 @@
 //# Classes de Exceções Customizadas que estendem Error (ex: APIError, LocalBoxError).
+
+export class APIError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "APIError";
+  }
+}
+
+export class LocalBoxError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "LocalBoxError";
+  }
+}
