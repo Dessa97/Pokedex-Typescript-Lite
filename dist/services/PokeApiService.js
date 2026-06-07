9620288a3d63551d9731c10bb5d5f7e47bb54247
Dessa97 <andressaoliveira862@gmail.com>
2026-06-07 12:20:32 -0300

    feat: Implementado o PokeApiService para obter dados de Pokémon com tratamento de erros.

---
+++ dist/services/PokeApiService.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PokeApiService = void 0;
+//# Camada de Integração Externa (fetch nativo). Retorna Promises tipadas com Interfaces.
+const CustomErrors_1 = require("../models/CustomErrors");
+class PokeApiService {
+    async buscarPokemon(nomeOuId) {
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${nomeOuId}`);
+            if (!response.ok) {
+                throw new CustomErrors_1.APIError(`Erro ao buscar Pokémon`);
+            }
+            const dados = await response.json();
+            return {
+                id: dados.id,
+                nome: dados.name,
+                tipos: dados.types.map((t) => t.type.name),
+                altura: dados.height,
+                peso: dados.weight,
+            };
+        }
+        catch (error) {
+            if (error instanceof CustomErrors_1.APIError) {
+                throw error;
+            }
+            throw new CustomErrors_1.APIError("Falha de comunicação com a API");
+        }
+    }
+}
+exports.PokeApiService = PokeApiService;
+//# sourceMappingURL=PokeApiService.js.map
